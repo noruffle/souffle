58e5cd73725b44cfb4a5dbd7586b2764a9999a05
Ruffle <noruffle@gmail.com>
2024-04-14 21:25:00 +0200

    upd

--- src/controllers/authorization/sign_in.rs
+++ src/controllers/authorization/sign_in.rs
@@ -1,21 +1,16 @@
-//Login to the account
-#[post("/lifecycle/signin", format = "json", data = "<ctx>")]
-pub fn sign_in(database: &State<Database>, ctx: Json<User>) -> Result<Json<InsertOneResult>, Status>
-{
-  let data = User {
-    id: None,
-    name: ctx.name.to_owned(),
-    email: ctx.email.to_owned(),
-    password: ctx.password.to_owned(),
-  };
-
-  
-}
-
-
+use mongodb::bson::doc;
 use rocket::{http::Status, serde::json::Json, State};
 use mongodb::results::InsertOneResult;
 
 use crate::services::database::authorization::Authorization;
 use crate::services::database::Database;
 use crate::models::user::User;
+
+//Login to the account
+#[post("/lifecycle/signin", format = "json", data = "<ctx>")]
+pub fn sign_in(database: &State<Database>, ctx: Json<User>) -> Result<Json<InsertOneResult>, Status>
+{
+  
+  
+}
+
--- src/services/database/authorization.rs
+++ src/services/database/authorization.rs
@@ -1,18 +1,51 @@
+use argon2::password_hash::Salt;
+use argon2::*;
+use mongodb::bson::{doc, oid::ObjectId};
+use rocket::{serde::json::Json, State};
+
+use crate::models::user::User;
+
 use super::Database;
 
 pub trait Authorization {
-  fn sign_up();
-  fn sign_in();
+  fn sign_up(db: &State<Connection>, username: String, email: String, password: String) -> Result<Json<User>>;
+  async fn sign_in(&self, database: &State<Database>, ctx: Json<User>);
 }
 
 impl Authorization for Database {
-  fn sign_up() 
-  {
-      
+  fn sign_up(db: &State<Database>, name: String, email: String, password: String) -> Result<Json<User>> {
+    let argon2 = Argon2::default();
+    let salt = Salt::from_b64("somesalt").unwrap();
+    
+    let res = argon2.hash_password_customized(password, None, None, Params::default(), salt).unwrap();
+
+    let user = User {
+        id: Some(ObjectId::new()),
+        name,
+        password: res,
+        email,
+    };
+
+    db.collection.insert_one(user, None);
+
+    Ok(Json(user))
   }
 
-  fn sign_in() 
+  async fn sign_in(&self, database: &State<Database>, ctx: Json<User>) //-> Result<Json<User>, Error>
   {
-    
+    let user = db.collection("users").find_one(doc! { "username": username }, None)?;
+
+    match user {
+        Some(user) => {
+            let password_matches = argon2::argon2id::verify(&user["password_hash"], password.as_bytes())?;
+
+            if password_matches {
+                Ok(Json(user))
+            } else {
+                Err("Incorrect password".to_string())
+            }
+        }
+        None => Err("User not found".to_string()),
+    }
   }
 }
